4fface54f5c41dfdf89b4f226bd170949b239220
federeggiani <elfeffe@gmail.com>
2024-01-29 20:52:29 -0300

    Update FilamentCodeEditorServiceProvider.php
    Add compatibility with Filament 3

--- src/FilamentCodeEditorServiceProvider.php
+++ src/FilamentCodeEditorServiceProvider.php
@@ -5,22 +5,22 @@ namespace SebastiaanKloos\FilamentCodeEditor;
 use Filament\Facades\Filament;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 
 class FilamentCodeEditorServiceProvider extends PackageServiceProvider
 {
-    protected array $beforeCoreScripts = [
-        'filament-tools' => __DIR__.'/../dist/filament-tools.js',
-    ];
-
     public function configurePackage(Package $package): void
     {
-        $package->name('filament-code-editor')->hasViews();
+        $package->name('filament-code-editor');
     }
 
     public function bootingPackage()
     {
-        Filament::serving(function () {
-            Filament::registerScripts($this->beforeCoreScripts, true);
-        });
+
+        FilamentAsset::register([
+            Js::make('code-mirror', __DIR__.'/../dist/filament-tools.js'),
+        ]);
+
     }
 }
